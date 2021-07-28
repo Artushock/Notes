af8c1fb4f4c3aba8246fbe023545247a000c1de7
Artiom Ponomariov <artushock@gmail.com>
2021-07-28 12:22:28 +0300

    Added handling of navigation menu. Finally it's work)

--- app/src/main/java/com/artushock/notes/MainActivity.java
+++ app/src/main/java/com/artushock/notes/MainActivity.java
@@ -3,12 +3,15 @@ package com.artushock.notes;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,10 +19,11 @@ import androidx.fragment.app.FragmentTransaction;
 import com.artushock.notes.ui.AboutAppFragment;
 import com.artushock.notes.ui.EditNewFragment;
 import com.artushock.notes.ui.SettingsFragment;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static ArrayList<Note> noteItems = new ArrayList<>();
 
     @Override
@@ -35,7 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         initToolbar();
+        initNavigationMenu();
+    }
 
+    private void initNavigationMenu() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void initToolbar() {
@@ -66,6 +75,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch(itemId){
+            case R.id.app_settings_menu:
+                addFragment(new SettingsFragment());
+                break;
+            case R.id.about_app_menu:
+                addFragment(new AboutAppFragment());
+                break;
+            case R.id.add_note_menu:
+                addFragment(new EditNewFragment());
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
     private void addFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -77,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentTransaction.commit();
     }
+
 
 
 }
